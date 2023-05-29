82fba7c25b3ea6980c673687bcf46ffbb6fa51b2
Maleen <maleensag@gmail.com>
2023-05-30 00:56:33 +0530

    Created project initialization files

---
+++ main.py
@@ -0,0 +1,19 @@
+c = str(0)
+x = 0
+y = 0
+z = float(0)
+
+print("......................................."
+      "*** Welcome to M.O. Calculator ***"
+      "---------------------------------------"
+      "Choose Operator You Need..."
+      "1 = ")
+
+if x == 1:
+    print("hi")
+elif x == 2:
+    print ("Hi 2")
+elif x == 3:
+    print("Hi 3")
+else:
+    print("Nothing")
